804e49b69ca6d4540d8040167cfe1e8badf6b9ee
Tominous <18189490+Tominous@users.noreply.github.com>
2020-06-14 16:33:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ new Bot({
     owners: [], // YOUR USER ID IN HERE
     prefix: "?", // DEFAULT IS ?
     token: "", // YOUR BOT TOKEN  (DO NOT SHARE WITH ANYONE)
-    // embed: {} // on: {}, off: {} // refer to the readme
+     embed: {} // on: {}, off: {} // refer to the readme
     watch: [
         // The user/bot's ID, The full webhook URL, the guildID that you want the bot to watch for, the roleID and if you want it enabled.
-        // add(`USERID`, `WEBHOOK_URL`, `GUILD_ID`, {role: "ROLE_ID", enabled: true}),
+         add(`USERID`, `WEBHOOK_URL`, `GUILD_ID`, {role: "ROLE_ID", enabled: true}),
     ],
     presence: {
         status: "online",
